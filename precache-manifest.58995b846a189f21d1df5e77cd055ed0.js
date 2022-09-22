baac114fff32ca61f8cd8bf7bbbf0c785b3db5b3
Ansab Khan <ansabkhansk@gmail.com>
2022-09-22 20:03:07 +0530

    Updates

---
+++ precache-manifest.58995b846a189f21d1df5e77cd055ed0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "367418380b103a6b260b",
+    "url": "/calc/static/css/main.1a1932b1.chunk.css"
+  },
+  {
+    "revision": "367418380b103a6b260b",
+    "url": "/calc/static/js/main.36741838.chunk.js"
+  },
+  {
+    "revision": "7f43181f989c8f9b0eb6",
+    "url": "/calc/static/js/1.7f43181f.chunk.js"
+  },
+  {
+    "revision": "117ab63626df48b19938",
+    "url": "/calc/static/js/runtime~main.117ab636.js"
+  },
+  {
+    "revision": "52e686810101905709224aa806031de2",
+    "url": "/calc/index.html"
+  }
+];
